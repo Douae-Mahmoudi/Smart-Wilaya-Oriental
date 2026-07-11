b1d6155c803b4cfc58b76e18cdeb9612469d7918
Douae-Mahmoudi <mahmoudi.douae.23@ump.ac.ma>
2026-07-11 11:00:36 +0100

    update

--- ressource_service/src/test/java/com/wilaya/ressource_service/RessourceServiceApplicationTests.java
+++ ressource_service/src/test/java/com/wilaya/ressource_service/RessourceServiceApplicationTests.java
@@ -2,12 +2,15 @@ package com.wilaya.ressource_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "spring.security.oauth2.resourceserver.jwt.issuer-uri=http://localhost:9999/realms/test"
+})
 class RessourceServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
