b33bbea3002c9641066ffaf1a892441ec67fb2de
Douae-Mahmoudi <mahmoudi.douae.23@ump.ac.ma>
2026-07-15 11:39:01 +0100

    update

--- signalement_service/src/test/java/com/wilaya/signalement_service/SignalementServiceApplicationTests.java
+++ signalement_service/src/test/java/com/wilaya/signalement_service/SignalementServiceApplicationTests.java
@@ -1,13 +1,15 @@
 package com.wilaya.signalement_service;
 
+import com.wilaya.signalement_service.config.TestSecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(TestSecurityConfig.class)
 class SignalementServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
--- signalement_service/src/test/java/com/wilaya/signalement_service/config/SecurityConfigTest.java
+++ signalement_service/src/test/java/com/wilaya/signalement_service/config/SecurityConfigTest.java
@@ -4,20 +4,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.UUID;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
+@Import(TestSecurityConfig.class)
 class SecurityConfigTest {
 
     @Autowired
@@ -28,6 +25,7 @@ class SecurityConfigTest {
         mockMvc.perform(get("/signalements/123"))
                 .andExpect(status().isNotFound());
     }
+
     @Test
     void shouldDenyAccessToProtectedEndpointsWithoutAuth() throws Exception {
         mockMvc.perform(get("/signalements"))
